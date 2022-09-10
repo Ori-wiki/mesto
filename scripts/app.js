fee2daeb3cc5142809ca1491ed3ce4deb932acfa
Denis Kazakov <madarauchiha6969@yandex.ru>
2022-09-10 15:28:18 +0300

    ревью

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,33 @@
-
-// Открытие и закрытие попапа
-const editProfile = document.querySelector('#editProfile');
-const closeEditProfileButton = editProfile.querySelector('.popup__button-close');
-const openEditProfileButton = document.querySelector('.profile__info-edit-button');
-
-
-openEditProfileButton.addEventListener('click', () => {
-  editProfile.classList.remove('popup_close');
-})
-
-closeEditProfileButton.addEventListener('click', () => {
-  editProfile.classList.add('popup_close');
-})
-
-
-// Поля формы
-const profileInputName = editProfile.querySelector('#popup__name-input');
-const profileInputProfession = editProfile.querySelector('#popup__profession-input');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__button-close');
+const popupOpenButton = document.querySelector('.profile__info-edit-button');
+const profileInputName = popup.querySelector('#popup__name-input');
+const profileInputProfession = popup.querySelector('#popup__profession-input');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
+const popupForm = popup.querySelector('.popup__form');
 
 
-profileInputName.setAttribute('value', profileName.textContent);
-profileInputProfession.setAttribute('value', profileProfession.textContent);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+function openPopup() {
+  popup.classList.add('popup_opened');
+  profileInputName.value = profileName.textContent;
+  profileInputProfession.value = profileProfession.textContent;
+}
 
-
-//Редактирование имени и информации о себе
-const profileInputForm = editProfile.querySelector('.popup__form');
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = profileInputName.value
   profileProfession.textContent = profileInputProfession.value
-  editProfile.classList.add('popup_close');
+  closePopup();
 }
-profileInputForm.addEventListener('submit', formSubmitHandler);
+
+popupOpenButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
+
