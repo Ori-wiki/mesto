e0dd408da4d9dd7d43c6fae61f852ae864c86c7c
Denis Kazakov <madarauchiha6969@yandex.ru>
2022-09-11 13:22:48 +0300

    ревью2

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,20 @@
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__button-close');
 const popupOpenButton = document.querySelector('.profile__info-edit-button');
-const profileInputName = popup.querySelector('#popup__name-input');
-const profileInputProfession = popup.querySelector('#popup__profession-input');
+const profileInputName = popup.querySelector('.popup__input_data_name');
+const profileInputProfession = popup.querySelector('.popup__input_data_profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 const popupForm = popup.querySelector('.popup__form');
 
 function closePopup() {
   popup.classList.remove('popup_opened');
-
 }
+
 function openPopup() {
-  popup.classList.add('popup_opened');
   profileInputName.value = profileName.textContent;
   profileInputProfession.value = profileProfession.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function formSubmitHandler(evt) {
