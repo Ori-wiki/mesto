94e502e8b8a6184d10eb3c47fd6d7ccf566b397a
Denis Kazakov <madarauchiha6969@yandex.ru>
2023-01-30 22:39:49 +0300

    popup

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -1,96 +1,96 @@
 export default class Api {
-  constructor(data) {
-    this._baseUrl = data.serverUrl;
-    this._token = data.token;
+  constructor (data) {
+    this._baseUrl = data.serverUrl
+    this._token = data.token
   }
 
-  _checkResult(res) {
+  _checkResult (res) {
     if (res.ok) {
-      return res.json();
+      return res.json()
     } else {
       return Promise.reject(
         `Что-то пошло не так: Ошибка ${res.status} - ${res.statusText}`
-      );
+      )
     }
   }
 
-  getCards() {
+  getCards () {
     return fetch(`${this._baseUrl}cards/`, {
       headers: {
-        authorization: this._token,
-      },
-    }).then((res) => this._checkResult(res));
+        authorization: this._token
+      }
+    }).then(res => this._checkResult(res))
   }
-  postCard(data) {
+  postCard (data) {
     return fetch(`${this._baseUrl}cards`, {
-      method: "POST",
+      method: 'POST',
       headers: {
         authorization: this._token,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         name: data.name,
-        link: data.link,
-      }),
-    }).then((res) => this._checkResult(res));
+        link: data.link
+      })
+    }).then(res => this._checkResult(res))
   }
 
-  deleteCard(id) {
+  deleteCard (id) {
     return fetch(`${this._baseUrl}cards/${id}`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        authorization: this._token,
-      },
-    }).then((res) => {
-      this._checkResult(res);
-    });
+        authorization: this._token
+      }
+    }).then(res => {
+      this._checkResult(res)
+    })
   }
-  getUserInfo() {
+  getUserInfo () {
     return fetch(`${this._baseUrl}users/me`, {
       headers: {
-        authorization: this._token,
-      },
-    }).then((res) => this._checkResult(res));
+        authorization: this._token
+      }
+    }).then(res => this._checkResult(res))
   }
-  patchUserInfo(data) {
+  patchUserInfo (data) {
     return fetch(`${this._baseUrl}users/me`, {
-      method: "PATCH",
+      method: 'PATCH',
       headers: {
         authorization: this._token,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         name: data.name,
-        about: data.profession,
-      }),
-    }).then((res) => this._checkResult(res));
+        about: data.profession
+      })
+    }).then(res => this._checkResult(res))
   }
-  patchUserAvatar(data) {
+  patchUserAvatar (data) {
     return fetch(`${this._baseUrl}users/me/avatar`, {
-      method: "PATCH",
+      method: 'PATCH',
       headers: {
         authorization: this._token,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        avatar: data.avatarLink,
-      }),
-    }).then((res) => this._checkResult(res));
+        avatar: data.avatarLink
+      })
+    }).then(res => this._checkResult(res))
   }
-  putLike(id) {
+  putLike (id) {
     return fetch(`${this._baseUrl}cards/${id}/likes`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        authorization: this._token,
-      },
-    }).then((res) => this._checkResult(res));
+        authorization: this._token
+      }
+    }).then(res => this._checkResult(res))
   }
-  deleteLike(id) {
+  deleteLike (id) {
     return fetch(`${this._baseUrl}cards/${id}/likes`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        authorization: this._token,
-      },
-    }).then((res) => this._checkResult(res));
+        authorization: this._token
+      }
+    }).then(res => this._checkResult(res))
   }
 }
